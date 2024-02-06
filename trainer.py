8908d636c97ac01c35e7ddf776972459a0896031
jinzr <zengrui.jin0@gmail.com>
2024-02-06 12:15:03 +0800

    updated

--- trainer.py
+++ trainer.py
@@ -1,32 +1,44 @@
+import argparse
+import logging
 import os
 import time
-import logging
-import argparse
 
-from utils.train import train
-from utils.hparams import HParam
-from utils.writer import MyWriter
 from datasets.dataloader import create_dataloader
+from utils.hparams import HParam
+from utils.train import train
+from utils.writer import MyWriter
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--base_dir', type=str, default='.',
-                        help="Root directory of run.")
-    parser.add_argument('-c', '--config', type=str, required=True,
-                        help="yaml file for configuration")
-    parser.add_argument('-e', '--embedder_path', type=str, required=True,
-                        help="path of embedder model pt file")
-    parser.add_argument('--checkpoint_path', type=str, default=None,
-                        help="path of checkpoint pt file")
-    parser.add_argument('-m', '--model', type=str, required=True,
-                        help="Name of the model. Used for both logging and saving checkpoints.")
+    parser.add_argument(
+        "-b", "--base_dir", type=str, default=".", help="Root directory of run."
+    )
+    parser.add_argument(
+        "-c", "--config", type=str, required=True, help="yaml file for configuration"
+    )
+    parser.add_argument(
+        "-e",
+        "--embedder_path",
+        type=str,
+        required=True,
+        help="path of embedder model pt file",
+    )
+    parser.add_argument(
+        "--checkpoint_path", type=str, default=None, help="path of checkpoint pt file"
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        required=True,
+        help="Name of the model. Used for both logging and saving checkpoints.",
+    )
     args = parser.parse_args()
 
     hp = HParam(args.config)
-    with open(args.config, 'r') as f:
+    with open(args.config, "r") as f:
         # store hparams as string
-        hp_str = ''.join(f.readlines())
+        hp_str = "".join(f.readlines())
 
     pt_dir = os.path.join(args.base_dir, hp.log.chkpt_dir, args.model)
     os.makedirs(pt_dir, exist_ok=True)
@@ -38,16 +50,17 @@ if __name__ == '__main__':
 
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
+        format="%(asctime)s - %(levelname)s - %(message)s",
         handlers=[
-            logging.FileHandler(os.path.join(log_dir,
-                '%s-%d.log' % (args.model, time.time()))),
-            logging.StreamHandler()
-        ]
+            logging.FileHandler(
+                os.path.join(log_dir, "%s-%d.log" % (args.model, time.time()))
+            ),
+            logging.StreamHandler(),
+        ],
     )
     logger = logging.getLogger()
 
-    if hp.data.train_dir == '' or hp.data.test_dir == '':
+    if hp.data.train_dir == "" or hp.data.test_dir == "":
         logger.error("train_dir, test_dir cannot be empty.")
         raise Exception("Please specify directories of data in %s" % args.config)
 
