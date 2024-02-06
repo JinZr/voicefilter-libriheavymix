e7578ad8aeb6b6f705f120788adb35c97bb657ef
jinzr <zengrui.jin0@gmail.com>
2024-02-06 11:59:25 +0800

    updated

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -1,8 +1,8 @@
-import os
-import glob
-import torch
+from random import choice
+
 import librosa
-from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.utils.data import DataLoader, Dataset
 
 from utils.audio import Audio
 
@@ -26,60 +26,105 @@ def create_dataloader(hp, args, train):
         return batch
 
     if train:
-        return DataLoader(dataset=VFDataset(hp, args, True),
-                          batch_size=hp.train.batch_size,
-                          shuffle=True,
-                          num_workers=hp.train.num_workers,
-                          collate_fn=train_collate_fn,
-                          pin_memory=True,
-                          drop_last=True,
-                          sampler=None)
+        return DataLoader(
+            dataset=VFDataset(hp, args, True),
+            batch_size=hp.train.batch_size,
+            shuffle=True,
+            num_workers=hp.train.num_workers,
+            collate_fn=train_collate_fn,
+            pin_memory=True,
+            drop_last=True,
+            sampler=None,
+        )
     else:
-        return DataLoader(dataset=VFDataset(hp, args, False),
-                          collate_fn=test_collate_fn,
-                          batch_size=1, shuffle=False, num_workers=0)
+        return DataLoader(
+            dataset=VFDataset(hp, args, False),
+            collate_fn=test_collate_fn,
+            batch_size=1,
+            shuffle=False,
+            num_workers=0,
+        )
 
 
 class VFDataset(Dataset):
     def __init__(self, hp, args, train):
-        def find_all(file_format):
-            return sorted(glob.glob(os.path.join(self.data_dir, file_format)))
+
+        def lines_to_dict(file_path):
+            res = dict()
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                key, value = line.strip().split()
+                res[key] = value
+            return res
+
+        def lines_to_dict_spk2src(file_path):
+            res = dict()
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                key, value = line.strip().split()
+                res[key] = value.split(",")
+            return res
+
+        def lines_to_dict_spk2spk(file_path):
+            res = dict()
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                key, value = line.strip().split()
+                wavid, spkid = key.split("_")
+                if wavid not in res:
+                    res[wavid] = dict()
+                    res[wavid][spkid] = value
+                else:
+                    res[wavid][spkid] = value
+            return res
+
         self.hp = hp
         self.args = args
         self.train = train
         self.data_dir = hp.data.train_dir if train else hp.data.test_dir
 
-        self.dvec_list = find_all(hp.form.dvec)
-        self.target_wav_list = find_all(hp.form.target.wav)
-        self.mixed_wav_list = find_all(hp.form.mixed.wav)
-        self.target_mag_list = find_all(hp.form.target.mag)
-        self.mixed_mag_list = find_all(hp.form.mixed.mag)
+        self.dvec_list = lines_to_dict_spk2src(hp.form.spk2src)
+        self.target_wav_list = lines_to_dict_spk2spk(hp.form.spk2spk)
+        self.mix2spk = lines_to_dict(hp.form.mix2spk)
+        self.mix2spk_keys = self.mix2spk.keys()
+        self.mixed_wav_list = lines_to_dict(hp.form.input)
 
-        assert len(self.dvec_list) == len(self.target_wav_list) == len(self.mixed_wav_list) == \
-            len(self.target_mag_list) == len(self.mixed_mag_list), "number of training files must match"
-        assert len(self.dvec_list) != 0, \
-            "no training file found"
+        assert (
+            len(self.dvec_list) == len(self.target_wav_list) == len(self.mixed_wav_list)
+        ), "number of training files must match"
+        assert len(self.dvec_list) != 0, "no training file found"
 
         self.audio = Audio(hp)
 
     def __len__(self):
-        return len(self.dvec_list)
+        return len(self.mix2spk_keys)
 
     def __getitem__(self, idx):
-        with open(self.dvec_list[idx], 'r') as f:
-            dvec_path = f.readline().strip()
+
+        mix_key = self.mix2spk_keys[idx]
+        mixed_path = self.mixed_wav_list[mix_key]
+        target_spk = self.mix2spk[mix_key]
+        target_path = self.target_wav_list[mix_key][target_spk]
+        dvec_path = choice(self.dvec_list[target_spk])
 
         dvec_wav, _ = librosa.load(dvec_path, sr=self.hp.audio.sample_rate)
         dvec_mel = self.audio.get_mel(dvec_wav)
         dvec_mel = torch.from_numpy(dvec_mel).float()
 
-        if self.train: # need to be fast
-            target_mag = torch.load(self.target_mag_list[idx])
-            mixed_mag = torch.load(self.mixed_mag_list[idx])
+        if self.train:  # need to be fast
+            target_mag, _ = self.wav2magphase(target_path)
+            mixed_mag, _ = self.wav2magphase(mixed_path)
             return dvec_mel, target_mag, mixed_mag
         else:
-            target_wav, _ = librosa.load(self.target_wav_list[idx], self.hp.audio.sample_rate)
-            mixed_wav, _ = librosa.load(self.mixed_wav_list[idx], self.hp.audio.sample_rate)
+            target_wav, _ = librosa.load(
+                self.target_wav_list[idx], self.hp.audio.sample_rate
+            )
+            mixed_wav, _ = librosa.load(
+                self.mixed_wav_list[idx], self.hp.audio.sample_rate
+            )
             target_mag, _ = self.wav2magphase(self.target_wav_list[idx])
             mixed_mag, mixed_phase = self.wav2magphase(self.mixed_wav_list[idx])
             target_mag = torch.from_numpy(target_mag)
